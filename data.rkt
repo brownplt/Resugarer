aecc517f3d0c3f661194b9f89105eb5ab7525c22
Justin <justinpombrio@cs.brown.edu>
2013-08-04 23:22:29 -0400

    .

---
+++ data.rkt
@@ -0,0 +1,43 @@
+(module data racket
+  (provide
+   (contract-out
+    (struct Node ((label symbol?) (terms list?)))
+    (struct List ((terms list?)))
+    (struct Tagged ((tags list?) (term any/c)))
+    (struct TermList ((tags list?) (terms list?)))
+    (struct TermAtom ((tags list?) (term any/c))))
+   (except-out (all-defined-out)
+    (struct-out Node)
+    (struct-out List)
+    (struct-out Tagged)
+    (struct-out TermList)
+    (struct-out TermAtom)))
+  
+  (require "utility.rkt")
+  (define (check-term-list? x)
+    (if (list? x) #t (fail "Boom! ~a" x)))
+
+  ; Common data structures
+    #| Tag ::= MacHead(macro-name, case-number, origin-term)
+             -- Marks root of macro-originating code.
+           | MacBody: Marks code that originated from a macro.
+           | Alien: Marks code that is not from here. |#
+  (struct MacHead (m c q) #:transparent)
+  (struct MacBody () #:transparent)
+  (struct Alien () #:transparent)
+  
+  #| AST ::= Node(Label, listof(AST))
+           | Tagged(listof(Tag), AST)
+           | Symbol
+           | Integer
+           | Float |#
+  (struct Node (label terms) #:transparent)
+  (struct List (terms) #:transparent)
+  (struct Tagged (tags term) #:transparent)
+  (struct CouldNotUnexpand ())
+
+  #| Term ::= TermList(listof(Tag), listof(Term))
+            | TaggedTerm(listof(Tag), Term) |#
+  (struct TermList (tags terms) #:transparent)
+  (struct TermAtom (tags term) #:transparent)
+)
